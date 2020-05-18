eb06ec35858619f65137792d520f88e51b8157dd
unknown <neerajkumar8546@gmail.com>
2020-05-18 10:07:22 +0530

    Cal_wage_Using_Switch

--- usingSwirchCase.sh
+++ usingSwirchCase.sh
@@ -1,13 +1,29 @@
 #!/bin/bash -x
 
+IS_PART_TIME=1
+IS_FULL_TIME=2
 MAX_HRS_IN_MONTH=4
 EMP_RATE_PER_HR=20
 NUM_WORKING_DAYS=20
 
 totalEmpHr=0
 totalworkingDays=0
-Part_Time_empHrs=8
 
+function getWorkHrs()
+{
+	case $1 in 
+		$IS_FULL_TIME)
+			empHrs=8
+		;;
+		$IS_PART_TIME)
+			empHrs=4
+		;;
+		*)
+			empHrs=0
+		;;
+	esac
+	echo $empHrs
+}
 
 function getEmpWage()
 {
@@ -18,7 +34,8 @@ while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalworkingDays -lt $NUM_WORKIN
 do
 	((totalworkingDays++))
 	empcheck=$((RANDOM%3))
-	totalEmpHrs=$(($totalEmpHrs+$Part_Time_empHrs))
+	empHrs="$(getWorkHrs $empcheck )"
+	totalEmpHrs=$(($totalEmpHrs+$empHrs))
 	dailyWages[$totalworkingDays]=$(($empHrs*$EMP_RATE_PER_HR))
 
 done
